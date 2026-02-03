0dc0b2978adfa816bf866ad8147fafe552127c0e
fatimap-3 <fatpatel@algomau.ca>
2026-02-03 16:31:35 -0500

    Changes study tips section

--- src/app/features/StudyTipFeature.java
+++ src/app/features/StudyTipFeature.java
@@ -4,10 +4,10 @@ public class StudyTipFeature {
     public static String run() {
         return "Study Tip\n"
              + "------------------------------\n"
-             + "Student Name: YOUR NAME HERE\n"
-             + "Study Style: YOUR STUDY STYLE HERE\n"
+             + "Student Name: Fatima Patel\n"
+             + "Study Style: I usually prefer writing my notes with pen on paper for better understanding\n"
              + "Tip:\n"
-             + "Replace this text with your own study tip.\n"
+             + "make notes for whatever you study.\n"
              + "------------------------------\n";
     }
 }
